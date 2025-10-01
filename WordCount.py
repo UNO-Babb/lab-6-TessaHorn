07c4c234807e5fc916dffd4ebec8d49153403f54
TessaHorn <86264560@nebraska.edu>
2025-10-01 13:42:52 -0500

    Update WordCount.py

--- WordCount.py
+++ WordCount.py
@@ -1,13 +1,30 @@
 #WordCount.py
-#Name:
-#Date:
-#Assignment:
+#Name: Tessa Horn
+#Date: 10/01/25
+#Assignment: Lab 6 Word Count
 
 def main():
   textFile = open("gettysberg.txt", 'r')
+  lineCount = 0
+  wordCount = 0
+  letterCount = 0
   
   for line in textFile:
-    print(line)
+    lineCount = lineCount + 1
+    words = line.split( )
+    for w in words:
+      wordCount = wordCount + 1
+      for l in w:
+        if l.isalpha():
+         letterCount = letterCount + 1
+      
+  
+      
+
+  print("Lines: ", lineCount)
+  print("Words: ", wordCount)
+  print("Letters: ", letterCount)
+
   
 
 if __name__ == '__main__':
